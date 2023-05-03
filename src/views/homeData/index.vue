00e7e508f14aee7af8795c59c35ab5c630f1fb6e
jack <104426438+gostrithe@users.noreply.github.com>
2023-05-04 02:09:12 +0800

    card

--- src/views/homeData/index.vue
+++ src/views/homeData/index.vue
@@ -1,37 +1,67 @@
 <template>
-  <div style="padding: 20px;">
+  <div style="padding: 20px">
     <h1>首页展示管理</h1>
     <el-collapse v-model="activeNames" @change="handleChange">
       <el-collapse-item title="轮播图管理" name="1">
         <template slot="title">
           <h2>轮播图管理</h2>
         </template>
-        <el-card style="width: 20%;" :body-style="{ padding: '20px',width: '100%' }">
-          <img
-            src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
-            class="image"
-          >
-          <div style="padding: 14px">
-            <span>好吃的汉堡</span>
-            <div class="bottom clearfix">
-              <el-button type="text" class="button">操作按钮</el-button>
+        <el-button style="margin-bottom: 10px;" round type="primary">从漫画库中选择</el-button>
+        <el-upload
+          action="https://jsonplaceholder.typicode.com/posts/"
+          list-type="picture-card"
+          :file-list="fileList"
+          :on-preview="handlePictureCardPreview"
+          :on-remove="handleRemove"
+        >
+          <i class="el-icon-plus" />
+        </el-upload>
+        <el-dialog :visible.sync="dialogVisible">
+          <div style="display: flex;">
+            <img width="50%" :src="dialogImageUrl" alt="">
+            <div style="margin-left: 40px;padding-top: 20px;">
+              <el-radio v-model="radio" label="1">漫画跳转</el-radio>
+              <el-radio v-model="radio" label="2">其他链接跳转</el-radio>
+              <el-divider />
+              <span>漫画ID:</span><el-input v-model="input" placeholder="请输入漫画ID" />
+              <el-divider />
+              <span>其他跳转链接:</span><el-input v-model="input2" placeholder="请输入跳转链接" />
+              <el-divider />
+              <div style="text-align: right;">
+                <el-button type="primary">确定</el-button>
+              </div>
             </div>
           </div>
-        </el-card>
+        </el-dialog>
       </el-collapse-item>
-      <el-collapse-item title="轮播图管理" name="1">
+      <el-collapse-item title="精品小说管理" name="2">
         <template slot="title">
           <h2>精品小说管理</h2>
         </template>
-        <el-card style="width: 20%;" :body-style="{ padding: '20px',width: '100%' }">
+        <el-card
+          style="width: 20%"
+          :body-style="{ padding: '20px', width: '100%' }"
+        >
           <img
             src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
             class="image"
           >
           <div style="padding: 14px">
-            <span>好吃的汉堡</span>
+            <div>漫画ID: 777</div>
+            <div>漫画名称: xxx</div>
+            <div>
+              描述：
+              <el-input
+                v-model="textarea1"
+                type="textarea"
+                autosize
+                placeholder="请输入内容"
+                readonly
+              />
+            </div>
             <div class="bottom clearfix">
-              <el-button type="text" class="button">操作按钮</el-button>
+              <el-button type="danger" class="button">删除</el-button>
+              <el-button type="info" class="button">编辑描述</el-button>
             </div>
           </div>
         </el-card>
@@ -44,12 +74,42 @@
 export default {
   data() {
     return {
-      activeNames: ['1']
+      textarea1: 'xxxxxxxxxxxxxxXXXXXXXXXXXXXXXXXX......',
+      input: '',
+      input2: '',
+      radio: '1',
+      activeNames: ['2'],
+      dialogImageUrl: '',
+      dialogVisible: false,
+      fileList: [
+        {
+          name: '轮播图1',
+          url: 'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png'
+        },
+        {
+          name: '轮播图2',
+          url: 'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png'
+        },
+        {
+          name: '轮播图3',
+          url: 'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png'
+        }
+      ]
     }
   },
   methods: {
+    onUploadSuccess(response, file, fileList) {
+      console.log(response, file, fileList)
+    },
     handleChange(val) {
       console.log(val)
+    },
+    handleRemove(file, fileList) {
+      console.log(file, fileList)
+    },
+    handlePictureCardPreview(file) {
+      this.dialogImageUrl = file.url
+      this.dialogVisible = true
     }
   }
 }
@@ -57,32 +117,34 @@ export default {
 
 <style lang="scss" scoped>
 .time {
-    font-size: 13px;
-    color: #999;
-  }
+  font-size: 13px;
+  color: #999;
+}
 
-  .bottom {
-    margin-top: 13px;
-    line-height: 12px;
-  }
+.bottom {
+  margin-top: 13px;
+  line-height: 12px;
+}
 
-  .button {
-    padding: 0;
-    float: right;
-  }
+.button {
+  padding: 2px;
+  float: right;
+  margin-left: 6px;
+}
 
-  .image {
-    width: 100%;
-    display: block;
-  }
+.image {
+  width: 100%;
+  height: 100px;
+  display: block;
+}
 
-  .clearfix:before,
-  .clearfix:after {
-      display: table;
-      content: "";
-  }
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
 
-  .clearfix:after {
-      clear: both
-  }
+.clearfix:after {
+  clear: both;
+}
 </style>
